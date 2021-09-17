127e0c90fe65fa1b55540aff4f31b345447c062a
sine/cosine/tangent <zfshmn@gmail.com>
2021-09-17 02:31:31 -0700

    CreateRecipe, UpdateRecipe, EnableRecipe, DisableRecipe, + exceptions

--- pylons_flutter/lib/src/exceptions.dart
+++ pylons_flutter/lib/src/exceptions.dart
@@ -11,6 +11,20 @@ class CookbookDoesNotExistException implements Exception {
         errMsg = e;
 }
 
+/// Exception thrown by TX-emitting API calls when a cookbook cannot be modified
+/// or a recipe cannot be created by the active profile because the cookbook is
+/// owned by a different one.
+class CookbookNotOwnedException implements Exception {
+  final String cookbook;
+  final String cbSender;
+  final String errMsg;
+
+  CookbookNotOwnedException(c, s, e) :
+        cookbook = c,
+        cbSender = s,
+        errMsg = e;
+}
+
 /// Exception thrown by API calls when a profile does not exist on the chain.
 class ProfileDoesNotExistException implements Exception {
   final String address;
@@ -30,6 +44,27 @@ class ProfileStateException implements Exception {
   final String errMsg;
 
   ProfileStateException(e) :
+        errMsg = e;
+}
+
+/// Exception thrown by TX-emitting API calls when a recipe already exists
+/// on chain at the coordinates one is trying to be created at.
+class RecipeAlreadyExistsException implements Exception {
+  final String cookbook;
+  final String recipeName;
+  final String recipeId;
+  final String errMsg;
+
+  RecipeAlreadyExistsException(c, rN, e) :
+        cookbook = c,
+        recipeName = rN,
+        recipeId = "",
+        errMsg = e;
+
+  RecipeAlreadyExistsException.constructorWithRecipeID(r, e) :
+        cookbook = "",
+        recipeName = "",
+        recipeId = r,
         errMsg = e;
 }
 
@@ -54,6 +89,41 @@ class RecipeDoesNotExistException implements Exception {
         errMsg = e;
 }
 
+/// Exception thrown by TX-emitting API calls when a recipe cannot be modified
+/// by the active profile because it's owned by a different one.
+class RecipeNotOwnedException implements Exception {
+  final String cookbook;
+  final String recipeName;
+  final String recipeId;
+  final String recipeSender;
+  final String errMsg;
+
+  RecipeNotOwnedException(c, rN, rI, s, e) :
+        cookbook = c,
+        recipeName = rN,
+        recipeId = rI,
+        recipeSender = s,
+        errMsg = e;
+}
+
+/// Exception thrown by TX-emitting API calls when a recipe is not in a valid
+/// state for the operation that the transaction attempted to perform.
+class RecipeStateException implements Exception {
+  final String cookbook;
+  final String recipeName;
+  final String recipeId;
+  final String errMsg;
+
+  RecipeStateException (c, rN, rI, e) :
+        cookbook = c,
+        recipeName = rN,
+        recipeId = rI,
+        errMsg = e;
+}
+
+/// Exception thrown by TX-emitting API calls when the Pylons node has an
+/// internal error during processing of the emitted TX. If this occurs in
+/// production environments, that's a problem.
 class NodeInternalErrorException implements Exception {
   final int errorCode;
   final String nodeErrMsg;
--- pylons_flutter/lib/src/wallet.dart
+++ pylons_flutter/lib/src/wallet.dart
@@ -19,7 +19,9 @@ class Wallet {
   /// is provided) or else with an exception. Since there's no normal way for
   /// this operation to fail, an exception will only be passed in the result of
   /// faulty behavior on the part of this library.
-  void getCookbooks(Function(Exception? exception, List<Cookbook>? cookbooks) callback) {
+  void getCookbooks(Function(
+      Exception? exception,
+      List<Cookbook>? cookbooks) callback) {
 
   }
 
@@ -29,7 +31,9 @@ class Wallet {
   /// was successfully retrieved, or else with an exception.
   /// If the profile simply doesn't exist, exception will be a
   /// ProfileNotFoundException; otherwise, it's whatever was thrown.
-  void getProfile(String? address, Function(Exception? exception, Profile? profile) callback) {
+  void getProfile(String? address, Function(
+      Exception? exception,
+      Profile? profile) callback) {
 
   }
 
@@ -42,7 +46,9 @@ class Wallet {
   /// is provided) or else with an exception. Since there's no normal way for
   /// this operation to fail, an exception will only be passed in the result of
   /// faulty behavior on the part of this library.
-  void getRecipes(String? address, Function(Exception? exception, List<Recipe>? recipes) callback) {
+  void getRecipes(String? address, Function(
+      Exception? exception,
+      List<Recipe>? recipes) callback) {
 
   }
 
@@ -52,12 +58,108 @@ class Wallet {
   /// is provided) or else with an exception. Since there's no normal way for
   /// this operation to fail, an exception will only be passed in the result of
   /// faulty behavior on the part of this library.
-  void getTrades(Function(Exception? exception, List<Trade>? trades) callback) {
+  void getTrades(Function(
+      Exception? exception,
+      List<Trade>? trades) callback) {
+
+  }
+
+  /// Creates a transaction to create the provided recipe on the Pylons chain
+  /// against the current profile.
+  /// A recipe must not already exist at the coordinates of the provided recipe.
+  /// Upon successful resolution of the transaction, calls callback with the
+  /// created transaction, the state of the profile after creation of the
+  /// recipe, and the recipe as it newly exists on chain as arguments. May,
+  /// instead, return one of several exceptions in the event that the
+  /// transaction is not successfully executed:
+  ///
+  /// RecipeValidationException: No TX was created because the recipe failed
+  /// basic clientside sanity-checking before one could be built. This could
+  /// be because of impossible outputs, etc. The error message provides more
+  /// info on why the sanity check failed.
+  ///
+  /// CookbookNotOwnedException: TX rejected because the active profile is not
+  /// the owner of the cookbook in which the recipe was to be created.
+  ///
+  /// RecipeAlreadyExistsException: TX rejected because recipe already exists
+  /// at the provided coordinates.
+  ///
+  /// ProfileStateException: The active profile was not able to create the
+  /// recipe on account of insufficient funds.
+  ///
+  /// ProfileDoesNotExistException: TX rejected because profile doesn't exist
+  /// on the chain.
+  ///
+  /// NodeInternalErrorException: TX rejected because the Pylons node had an
+  /// internal error. This shouldn't be seen in production.
+  ///
+  /// If the operation fails due to an exception thrown by this library, that
+  /// exception will be passed directly.
+  void txCreateRecipe(Recipe recipe, Function(
+      Exception? exception,
+      Transaction? tx,
+      Profile? profile,
+      Recipe? recipe
+  ) callback) {
+
+  }
+
+  /// Creates a transaction to disable the recipe with the provided real,
+  /// on-chain recipe ID against the current profile. Upon successful
+  /// resolution of the transaction, calls callback with the created transaction
+  /// as an argument. May, instead, return one of several exceptions in the event
+  /// that the transaction is not successfully executed:
+  ///
+  /// RecipeDoesNotExistException: TX rejected because recipe does not exist.
+  ///
+  /// RecipeNotOwnedException: TX rejected because the active profile is not
+  /// the owner of the recipe.
+  ///
+  /// RecipeStateException: TX rejected because the recipe is already disabled.
+  ///
+  /// ProfileDoesNotExistException: TX rejected because profile doesn't exist
+  /// on the chain.
+  ///
+  /// NodeInternalErrorException: TX rejected because the Pylons node had an
+  /// internal error. This shouldn't be seen in production.
+  ///
+  /// If the operation fails due to an exception thrown by this library, that
+  /// exception will be passed directly.
+  void txDisableRecipe (String recipeId, Function(
+      Exception? exception,
+      Transaction? tx) callback) {
+
+  }
+
+  /// Creates a transaction to enable the recipe with the provided real,
+  /// on-chain recipe ID against the current profile. Upon successful
+  /// resolution of the transaction, calls callback with the created transaction
+  /// as an argument. May, instead, return one of several exceptions in the event
+  /// that the transaction is not successfully executed:
+  ///
+  /// RecipeDoesNotExistException: TX rejected because recipe does not exist.
+  ///
+  /// RecipeNotOwnedException: TX rejected because the active profile is not
+  /// the owner of the recipe.
+  ///
+  /// RecipeStateException: TX rejected because the recipe is already enabled.
+  ///
+  /// ProfileDoesNotExistException: TX rejected because profile doesn't exist
+  /// on the chain.
+  ///
+  /// NodeInternalErrorException: TX rejected because the Pylons node had an
+  /// internal error. This shouldn't be seen in production.
+  ///
+  /// If the operation fails due to an exception thrown by this library, that
+  /// exception will be passed directly.
+  void txEnableRecipe (String recipeId, Function(
+      Exception? exception,
+      Transaction? tx) callback) {
 
   }
 
   /// Creates a transaction to execute the recipe with coordinates
-  /// cookbookId:recipeId against the current profile. Upon successful
+  /// cookbookId:recipeName against the current profile. Upon successful
   /// resolution of the transaction, calls callback with the created transaction
   /// and the state of the active profile after execution of the recipe as
   /// arguments. May, instead, return one of several exceptions in the event
@@ -78,13 +180,47 @@ class Wallet {
   ///
   /// If the operation fails due to an exception thrown by this library, that
   /// exception will be passed directly.
-  void txExecuteRecipe (String cookbookId, String recipeId,
+  void txExecuteRecipe (String cookbookId, String recipeName,
       Function(Exception? exception,
           Transaction? tx,
           Profile? profile) callback) {
 
   }
 
+  /// Creates a transaction to update an extant recipe on the Pylons chain
+  /// to conform to the one provided against the current profile.
+  /// A recipe must already exist at the coordinates of the provided recipe.
+  /// Upon successful resolution of the transaction, calls callback with the
+  /// created transaction, the state of the profile after updating the
+  /// recipe, and the recipe as it now exists on chain as arguments. May,
+  /// instead, return one of several exceptions in the event that the
+  /// transaction is not successfully executed:
+  ///
+  /// RecipeValidationException: No TX was created because the recipe failed
+  /// basic clientside sanity-checking before one could be built. This could
+  /// be because of impossible outputs, etc. The error message provides more
+  /// info on why the sanity check failed.
+  ///
+  /// RecipeNotOwnedException: TX rejected because the active profile is not
+  /// the owner of the recipe to be updated.
+  ///
+  /// RecipeDoesNotExistException: TX rejected because the recipe to be
+  /// updated does not exist on the Pylons chain.
+  ///
+  /// ProfileDoesNotExistException: TX rejected because profile doesn't exist
+  /// on the chain.
+  ///
+  /// NodeInternalErrorException: TX rejected because the Pylons node had an
+  /// internal error. This shouldn't be seen in production.
+  ///
+  /// If the operation fails due to an exception thrown by this library, that
+  /// exception will be passed directly.
+  void txUpdateRecipe(Recipe recipe, Function(
+      Exception? exception,
+      Transaction? tx,
+      Profile? profile,
+      Recipe? recipe
+      ) callback) {
 
-
+  }
 }
