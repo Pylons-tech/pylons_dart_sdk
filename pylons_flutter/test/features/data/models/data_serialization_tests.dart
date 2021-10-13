ef961bfb199fbfabafc4cc373446496b5c63459a
sine/cosine/tangent <zfshmn@gmail.com>
2021-10-12 23:05:10 -0700

    Test stubs for data serialization/deserialization tests

---
+++ pylons_flutter/test/features/data/models/data_serialization_tests.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_test/flutter_test.dart';
+
+/// data_serialization_tests contains tests that verify that our data-model types serialize and deserialize to the same structures as
+/// those expected by the wallet and the node.
+void main() {
+  group('Cookbook', () {
+    test('Cookbook type seriializes correctly', () {
+      throw UnimplementedError();
+    });
+    test('Cookbook JSON from remote deserializes correctly', () {
+      throw UnimplementedError();
+    });
+  });
+  group('Recipe', () {
+    test('Recipe type seriializes correctly', () {
+      throw UnimplementedError();
+    });
+    test('Recipe JSON from remote deserializes correctly', () {
+      throw UnimplementedError();
+    });
+  });
+  group('Trade', () {
+    test('Trade type seriializes correctly', () {
+      throw UnimplementedError();
+    });
+    test('Trade JSON from remote deserializes correctly', () {
+      throw UnimplementedError();
+    });
+  });
+}
