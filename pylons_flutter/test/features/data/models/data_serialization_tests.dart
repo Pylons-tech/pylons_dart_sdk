22157fc70d785e0e6469d2838c3319390f61e95f
sine/cosine/tangent <zfshmn@gmail.com>
2021-10-13 01:15:43 -0700

    Cookcook deseriailiazation test + naming fix to make it pass

--- pylons_flutter/test/features/data/models/data_serialization_tests.dart
+++ pylons_flutter/test/features/data/models/data_serialization_tests.dart
@@ -1,18 +1,34 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:pylons_flutter/pylons_flutter.dart';
+
+import '../../../helper/test_util.dart';
 
 /// data_serialization_tests contains tests that verify that our data-model types serialize and deserialize to the same structures as
 /// those expected by the wallet and the node.
 void main() {
   group('Cookbook', () {
-    test('Cookbook type seriializes correctly', () {
+    test('Cookbook type serializes correctly', () {
       throw UnimplementedError();
     });
     test('Cookbook JSON from remote deserializes correctly', () {
-      throw UnimplementedError();
+      late final Cookbook cb;
+      expect(() => cb = TestUtil.loadCookbook('cookbook/cookbook_from_node.json'), returnsNormally,
+          reason: 'Deserialization failed');
+      assert(cb.enabled == true);
+      assert(cb.name == 'Legend of the Undead Dragon');
+      assert(cb.costPerBlock.amount == '1000000');
+      assert(cb.costPerBlock.denom == 'upylon');
+      assert(cb.creator == 'pylo1akzpu26f36pgxr636uch8evdtdjepu93v5y9s2');
+      assert(cb.developer == 'Pylons Inc');
+      assert(cb.description == 'Cookbook for running pylons recreation of LOUD');
+      assert(cb.ID == 'cookbookLOUD');
+      assert(cb.nodeVersion == 'v0.1.3');
+      assert(cb.supportEmail == 'alex@shmeeload.xyz');
+      assert(cb.version == 'v0.0.1');
     });
   });
   group('Recipe', () {
-    test('Recipe type seriializes correctly', () {
+    test('Recipe type serializes correctly', () {
       throw UnimplementedError();
     });
     test('Recipe JSON from remote deserializes correctly', () {
@@ -20,7 +36,7 @@ void main() {
     });
   });
   group('Trade', () {
-    test('Trade type seriializes correctly', () {
+    test('Trade type serializes correctly', () {
       throw UnimplementedError();
     });
     test('Trade JSON from remote deserializes correctly', () {
